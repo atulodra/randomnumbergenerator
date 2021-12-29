88c253744501b8dda3e05b029c29ece5d63a9476
Atul Shrestha <atulshrez@gmail.com>
2021-12-29 23:42:36 +0545

    first commit - added random number generator header file

---
+++ randomnumbergenerator.h
@@ -0,0 +1,13 @@
+#ifndef randomnumbergenerator_h
+#define randomnumbergenerator_h
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void randomNumberGenerator(int *array, int size){
+    srand(time(NULL));
+    for (int i=0;i<size;i++)
+        array[i] = rand();
+}
+
+#endif
